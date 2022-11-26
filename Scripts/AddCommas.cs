2926bcdb4191a5c028a3ee2ae814b9cc474df6cc
kassassin99 <kassassin99@gmail.com>
2022-11-26 10:42:36 +0530

    Script to add commas to numbers in indian and international style.

---
+++ Scripts/AddCommas.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Test : MonoBehaviour
+{
+  public long num = 12345678;
+
+
+  void Start()
+  {
+
+	string numberString = num.ToString();
+
+    string resultString = AddCommasIndian(numberString);
+
+    Debug.Log(resultString);
+  }
+
+  public string AddCommas(string numberString)
+  {
+    string str = numberString;
+    int count = numberString.Length;
+    string result = "";
+
+    for (int i = count - 1; i >= 0; i--)
+    {
+      string temp = str.Substring(i, 1);
+      if ((count - 1) - i != 0 && ((count - 1) - i) % 3 == 0)
+      {
+        result = temp + "," + result;
+      }
+      else
+      {
+        result = temp + result;
+      }
+    }
+
+    return result;
+  }
+
+  public string AddCommasIndian(string numberString)
+  {
+    string str = numberString;
+    int count = numberString.Length;
+    string result = "";
+
+    string lastThree = numberString.Substring(count - 4, 3);
+
+    for (int i = count - 4; i >= 0; i--)
+    {
+      string temp = str.Substring(i, 1);
+      if (((count - 4) - i) % 2 == 0)
+      {
+        result = temp + "," + result;
+      }
+      else
+      {
+        result = temp + result;
+      }
+    }
+
+    result = result + lastThree;
+
+    return result;
+  }
+}
